67c9b8930f7202814a49c4d19346c86d1c269704
fabezio <fabezio@outlook.fr>
2020-01-17 20:41:52 +0100

    ajout de la page de progression

---
+++ pages/progress.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+div
+  h1.text-center Page de progression
+  p Ce site est en perpetuelle évolution; voici ce qu'il est prévu:
+  ul
+    li adaptation responsive
+    li dynamisation
+    li inclusion horloge numérique
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+li {
+  list-style: '> ';
+}
+</style>
