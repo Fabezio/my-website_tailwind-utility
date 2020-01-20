bbe914157f08083914e93240c8d4cfecd361def8
fabezio <fabezio@outlook.fr>
2020-01-20 13:32:35 +0100

    mise a jour about

--- pages/about.vue
+++ pages/about.vue
@@ -2,13 +2,20 @@
   div#author
     h1.title.text-center Qui je suis
     article.text-justified
-      p Je suis Fabezio, développeur Web orienté Javascript, plus exactement un codeur Javascript orienté web, spécialisé vueJS et nuxtJS.
+      p Je suis Fabezio, développeur Web orienté Javascript, plus exactement un codeur Javascript orienté web, spécialisé vueJS / nuxtJS.
       h2.subtitle.text-center.mt-4 Un peu d'histoire
       p J'ai débuté dans la vie active comme technicien en télécommunications dans le privé. Le secteur étant rapidement bouché, j'ai dû m'orienter vers une branche sans rapport avec ma formation initiale, mais où le travail ne manquait pas. J'officiais de nuit dans la sécurité, en statu quo durant de longues années, me demandant si j'étais encore apte à un travail constructif et plaisant.
-      p Mes vacations nocturnes me permirent de m'enrichir intellectuellement, de redécouvrir l'informatique, d'explorer l'envers du décor; j'adoptai rapidement Linux, l'OS la plus populaire des geeks, testant au passage Ubuntu, Mint, Manjaro, pour finir avec Fedora. 
-      br
-      | J'adoptai également fin 2019 Mac pour pouvoir tirer parti de sa puissance et sa fluidité.
-      p En réalisant la puissance du codage, je ne fus pas long à décider de suivre une longue et fastidieuse (mais Ô combien passionnante) formation de développement web, option FrontEnd. Depuis, plus je code, plus je cherche, plus je prends de plaisir.
+      p Mes vacations nocturnes me permirent de m'enrichir intellectuellement.
+      p Puis je redécouvris l'informatique, Linux, bash, le codage python.
+      p En réalisant la puissance du codage, je ne fus pas long à décider de suivre une longue et fastidieuse (mais Ô combien passionnante) formation de développement web, option FrontEnd, avant de m'essayer au Backend.
+        | Déçu par PHP, je m'orientai vers le&nbsp;
+        strong
+          em tout JavaScript.&nbsp;
+      p Convaincu par la supériorité de Linux, je testai des distributions de diverses familles: Ubuntu, Mint, Manjaro, pour finir avec Fedora, laquelle me paraît pour le moment la plus indiquée dans ce domaine.
+        | Depuis, plus je code, plus je cherche, plus je prends de plaisir. J'utilise&nbsp;
+        em Visual Studio Code et Git&nbsp;
+        | quotidiennement et découvre des fonctions nouvelles chaque semaine.
+      p J'adoptai également Mac, fin 2019, pour pouvoir tirer parti de sa puissance et sa fluidité.
 
 </template>
 
